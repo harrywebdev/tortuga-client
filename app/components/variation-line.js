a0b39e4608dec4cd4d7f3d2a335f4b3a0bd0bd84
Harry B <harry@gethornet.com>
2019-06-07 10:25:03 +0200

    refactor: Variation line component

--- app/components/variation-line.js
+++ app/components/variation-line.js
@@ -1,36 +1,27 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default Component.extend({
-    cart: service('cart'),
+export default class VariationLineComponent extends Component {
+    @service cart;
 
-    tagName: 'li',
+    tagName = 'li';
 
-    classNames: ['list-group-item', 'variation-line'],
-    classNameBindings: ['countInCart:list-group-item-primary'],
+    classNames = ['list-group-item', 'variation-line'];
+    classNameBindings = ['countInCart:list-group-item-primary'];
 
-    // for Order Item
-    readonly: false,
-
-    click() {
-        if (this.readonly === true) {
-            return;
-        }
-        this.cart.addToCart(this.get('variation.id'));
-    },
-
-    countInCart: computed('cart.items.[]', function() {
-        if (this.readonly === true) {
-            return false;
-        }
-
+    @computed('cart.items.[]', 'variation.id')
+    get countInCart() {
         return this.cart.howMuchOf(this.get('variation.id'));
-    }),
+    }
 
-    actions: {
-        removeFromCart() {
-            this.cart.removeFromCart(this.get('variation.id'));
-        },
-    },
-});
+    @action
+    addToCart() {
+        this.cart.addToCart(this.get('variation.id'));
+    }
+
+    @action
+    removeFromCart() {
+        this.cart.removeFromCart(this.get('variation.id'));
+    }
+}
