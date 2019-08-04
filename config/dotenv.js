740cc0457a600343fd0c243de5cd45d0b699eb93
Harry B <harry@gethornet.com>
2019-08-04 22:47:51 +0200

    fix: Environment in Dotenv when deploying

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,13 @@ const path = require('path');
 
 module.exports = function(env) {
     return {
-        clientAllowedKeys: [],
+        clientAllowedKeys: [
+            'API_HOST',
+            'ACCOUNTKIT_APP_ID',
+            'ACCOUNTKIT_DEBUG',
+            'ACCOUNTKIT_REDIRECT_URL',
+            'ACCOUNTKIT_VERSION',
+        ],
         fastbootAllowedKeys: [],
         failOnMissingKey: true,
         path: path.join(path.dirname(__dirname), `.env.${env}`),
